0b6318f98a2c05f8e4bcafdfe94bc4871978ba2f
Ân Đinh - Sandmark <hongan12@gmail.com>
2018-02-05 10:54:55 +0700

    Update meteor server config for loginTokenExpires

--- api/server/main.ts
+++ api/server/main.ts
@@ -2,9 +2,11 @@ import {Meteor} from 'meteor/meteor';
 
 Meteor.startup(() => {
   if (Meteor.isServer) {
+    Accounts.config({
+      loginExpirationInDays: null
+    });
     process.env.MAIL_URL = 'smtps://nsa1667%40gmail.com:65312%23%26gM@smtp.gmail.com:465/';
   }
-
   Accounts.emailTemplates.siteName = 'FTEL Notes Tm.';
   Accounts.emailTemplates.from = 'ftelnotes@fpt.com';
   Accounts.emailTemplates.resetPassword = {
